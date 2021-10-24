41aa3111570708e2d93297859299f53182d39150
Ameen671999 <ameen671999@gmail.com>
2021-10-24 13:14:19 +0530

    Fetch API in React

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,10 +9,11 @@ export class NewsItem extends Component {
       <>
       <div className="my-3">
       <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={imageUrl?imageUrl:'https://cdn.modernghana.com/story_/770/400/728202075440-k5fqi7t2h0-d9aa159b8be12202613494d3640ebcf9.jpg'} 
+          className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a href={newsUrl} target="_url" className="btn btn-sm btn-primary">
               Read More
             </a>
